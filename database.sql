fab975e2c9812bb84580d890890960182661bcf6
Robbie Constantin <robbieconstantin@gmail.com>
2013-12-12 11:28:50 -0500

    initial DB file

---
+++ database.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 12, 2013 at 05:21 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.4.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `silvousp_p4_silvousplais_biz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE `playlists` (
+  `playlist_id` int(11) NOT NULL AUTO_INCREMENT,
+  `playlist_name` varchar(255) NOT NULL,
+  `created` int(11) NOT NULL,
+  PRIMARY KEY (`playlist_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` int(11) NOT NULL,
+  `modified` int(11) NOT NULL,
+  `IP` varchar(255) NOT NULL,
+  `country` varchar(2) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `registration_code` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `last_login` int(11) NOT NULL,
+  `timezone` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `avatarUrl` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `users`
+--
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_users`
+--
+
+CREATE TABLE `users_users` (
+  `user_user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_id_followed` int(11) NOT NULL,
+  PRIMARY KEY (`user_user_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `users_users`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `video_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `thumbnail_url` varchar(255) NOT NULL,
+  `yt_video_id` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `playlist_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created` int(11) NOT NULL,
+  `last_played` int(11) NOT NULL,
+  PRIMARY KEY (`video_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `videos`
+--
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_users`
+--
+ALTER TABLE `users_users`
+  ADD CONSTRAINT `users_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `videos`
+  ADD CONSTRAINT `videos_ibfk_2` FOREIGN KEY (`playlist_id`) REFERENCES `playlists` (`playlist_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
